c30888fa64fd5234ce73a1a045a4feea9e885bba
xeonv3 <super163mail@163.com>
2013-07-16 21:19:39 +0800

    Game9-Space out2
    为游戏开始时添加一个片头演示，显示游戏最高的前5位分数记录。

---
+++ GAME_LEARNING-Space Out2/GAME_LEARNING/AlienSprite.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<Windows.h>
+#include "Sprite.h"
+
+class AlienSprite: public Sprite{
+public:
+	AlienSprite(Bitmap* pBitmap,RECT& rcBounds,BOUNDSACTION baBoundsAction = BA_STOP);
+	virtual ~AlienSprite();
+	
+	virtual SPRITEACTION Update();
+	virtual Sprite* AddSprite();
+
+};
